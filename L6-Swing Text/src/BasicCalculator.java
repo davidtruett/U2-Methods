877a1a5392456d477286670a4c3e9daf7130a050
truetdav000 <truetdav000@warren.k12.in.us>
2021-09-30 11:40:29 -0400

    2.6

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class BasicCalculator {
 
     /* Create a calculator program with two text fields
@@ -8,4 +12,95 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
+    static JTextField inputField1;
+    static JTextField inputField2;
+    static JLabel resultLabel;
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("calculator");
+        JPanel panel = new JPanel();
+
+        JLabel inputLabel1 = new JLabel("");
+        JLabel inputLabel2 = new JLabel("");
+        inputField1 = new JTextField(18);
+        inputField2 = new JTextField(18);
+
+        JButton add = new JButton("+");
+        JButton subt = new JButton("-");
+        JButton mult = new JButton("*");
+        JButton div = new JButton("/");
+
+        JLabel resultLabel = new JLabel("0.00");
+
+
+        window.setSize(250,400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        add.addActionListener(new add());
+
+        panel.add(inputField1);
+        panel.add(inputLabel1);
+        panel.add(inputField2);
+        panel.add(inputLabel2);
+
+        panel.add(resultLabel);
+
+        panel.add(add);
+        panel.add(subt);
+        panel.add(mult);
+        panel.add(div);
+
+
+        window.add(panel);
+
+        window.setVisible(true);
+
+        double num1 = Double.parseDouble(String.valueOf(inputField1));
+        double num2 = Double.parseDouble(String.valueOf(inputField2));
+
+        System.exit(0);
+
+    }
+    private static class add implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+            resultLabel.setText(String.valueOf(num1 + num2));
+
+        }
+
+    }
+    private static class subt implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+            resultLabel.setText(String.valueOf(num1 - num2));
+
+        }
+
+    }
+    private static class mult implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+            resultLabel.setText(String.valueOf(num1 * num2));
+
+        }
+
+    }
+    private static class div implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            double num1 = Double.parseDouble(inputField1.getText());
+            double num2 = Double.parseDouble(inputField2.getText());
+
+            resultLabel.setText(String.valueOf(num1 / num2));
+
+        }
+
+    }
+
 }
--- L6-Swing Text/src/SimpleInput.java
+++ L6-Swing Text/src/SimpleInput.java
@@ -1,4 +1,49 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class SimpleInput {
+
+    static int clickCount = 0;
+    static JLabel clickCountLabel;
+    static JLabel explosion;
+    static JTextField input;
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("clicker explosion");
+        JPanel panel = new JPanel();
+        clickCountLabel = new JLabel("click count: ");
+        explosion = new JLabel();
+        JLabel inputLabel = new JLabel("enter product name: ");
+        input = new JTextField(15);
+        JButton button = new JButton("click4explosion");
+
+        window.setSize(250,400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        button.addActionListener(new ButtonListener());
+
+        panel.add(inputLabel);
+        panel.add(input);
+        panel.add(clickCountLabel);
+        panel.add(button);
+
+        window.add(panel);
+
+        window.setVisible(true);
+
+    }
+    private static class ButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            String product = input.getText();
+            clickCount++;
+            clickCountLabel.setText("Sold: " + clickCount + " number of " + product);
+
+
+        }
+
+    }
 
 
 }
